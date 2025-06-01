d39c78235ffda274279ca7f7a4c81f38381551e4
Jvitorasantos <jv5alvessantos@gmail.com>
2025-05-31 23:43:01 -0300

    Ajusta Program do projeto de worker

--- ChatZapfy.Workers/Program.cs
+++ ChatZapfy.Workers/Program.cs
@@ -1,44 +1,91 @@
+
+using System.Reflection.Metadata;
+using ChatZapfy.Aplicacao.Usuarios.Profiles;
+using ChatZapfy.Aplicacao.Usuarios.Servicos;
+using ChatZapfy.Dominio.Usuarios.Servicos;
+using ChatZapfy.Infra.Usuarios.Mapeamentos;
+using ChatZapfy.Infra.Usuarios.Repositorios;
+using CrystalQuartz.AspNetCore;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Dialect;
+using Quartz;
+using Quartz.Impl;
+using Quartz.Impl.Matchers;
+using Quartz.Spi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+string connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+
+builder.Services.AddSingleton<ISessionFactory>(factory =>
+{
+    return Fluently.Configure()
+        .Database(MySQLConfiguration.Standard.Dialect<MySQL5Dialect>().ConnectionString(connectionString))
+        .Mappings(x => x.FluentMappings.AddFromAssemblyOf<UsuariosMap>())
+        .BuildSessionFactory();
+});
+
+builder.Services.AddScoped<NHibernate.ISession>(factory =>
+{
+    var sessionFactory = factory.GetService<ISessionFactory>();
+    return sessionFactory.OpenSession();
+});
+
+builder.Services.AddScoped<ITransaction>(factory =>
+{
+    var session = factory.GetService<NHibernate.ISession>();
+    return session.BeginTransaction();
+});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton<IJobFactory, ScheduledJobFactory>();
+builder.Services.AddSingleton<IJobListener, LogsJobListener>();
+
+
+
+builder.Services.AddAutoMapper(typeof(UsuariosProfile));
+
+builder.Services.Scan(scan => scan
+    .FromAssemblyOf<UsuariosAppServico>()
+        .AddClasses()
+            .AsImplementedInterfaces()
+                .WithScopedLifetime());
+
+builder.Services.Scan(scan => scan
+    .FromAssemblyOf<UsuariosServico>()
+    .AddClasses()
+        .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+builder.Services.Scan(scan => scan
+    .FromAssemblyOf<UsuariosRepositorio>()
+    .AddClasses()
+        .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+var schedulerFactory = new StdSchedulerFactory();
+var scheduler = schedulerFactory.GetScheduler().GetAwaiter().GetResult();
+scheduler.JobFactory = builder.Services.BuildServiceProvider().GetService<IJobFactory>();
+
+scheduler.ListenerManager.AddJobListener(builder.Services.BuildServiceProvider().GetService<IJobListener>(), GroupMatcher<JobKey>.AnyGroup());
+
+await scheduler.ScheduleJob(faturamentosJob, TriggerBuilder.Create().WithCronSchedule("0 * * ? * *").Build());
+
+await scheduler.Start();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseCrystalQuartz(() => scheduler);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
